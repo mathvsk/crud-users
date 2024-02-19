50a0fe34dc40036f99fa41844b38655b9346e280
Matheus Viscki <matheusviscki@gmail.com>
2024-02-19 07:43:13 -0300

    feat: create route get and post (users) - add stream concept - work with buffer manipulation

--- src/server.js
+++ src/server.js
@@ -2,9 +2,42 @@
 // ESModulo => import/export
 
 import http from "node:http";
+import buffer from "node:buffer";
 
-const server = http.createServer((req, res) => {
-    return res.end('Hello World!! funcinou?')
+const users = []
+
+const server = http.createServer(async (req, res) => {
+    const method = req.method;
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        req.body = {};
+    }
+
+    if (method === "GET" && req.url === "/users") {
+        return res.setHeader("Content-Type", "application/json").end(JSON.stringify(users));
+    }
+
+    if (method === "POST" && req.url === "/users") {
+        const { name, email } = req.body
+
+        users.push({
+            id: 1,
+            name,
+            email,
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
