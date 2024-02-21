366eda328358155ca9dca78f96e66896ca599e41
Matheus Viscki <matheusviscki@gmail.com>
2024-02-21 18:54:19 -0300

    refactor: create routes file

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@
 import http from "node:http";
 import {json} from "./middlewares/json.js";
 import {Database} from "./database.js";
-import {randomUUID} from "node:crypto";
+import {routes} from "./routes.js";
 
 const database = new Database()
 
@@ -13,24 +13,12 @@ const server = http.createServer(async (req, res) => {
 
     await json(req, res)
 
-    if (method === "GET" && req.url === "/users") {
-        const users = database.select('users');
+    const route = routes.find(route => (
+        route.path === req.url && route.method === method
+    ))
 
-        return res.end(JSON.stringify(users));
-    }
-
-    if (method === "POST" && req.url === "/users") {
-        const { name, email } = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
