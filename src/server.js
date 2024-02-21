a558b564c6126e0af8207cd6224550fe999fc417
Matheus Viscki <matheusviscki@gmail.com>
2024-02-21 19:25:21 -0300

    feat: create delete route - create function for delete a user from database - create utility to manipulate route parameters

--- src/server.js
+++ src/server.js
@@ -3,21 +3,21 @@
 
 import http from "node:http";
 import {json} from "./middlewares/json.js";
-import {Database} from "./database.js";
 import {routes} from "./routes.js";
 
-const database = new Database()
-
 const server = http.createServer(async (req, res) => {
-    const method = req.method;
+    const { method, url } = req
 
     await json(req, res)
 
     const route = routes.find(route => (
-        route.path === req.url && route.method === method
+        route.path.test(url) && route.method === method
     ))
 
     if (route) {
+        const routeParams = req.url.match(route.path)
+        req.params = { ...routeParams.groups }
+
         return route.handler(req, res)
     }
 
